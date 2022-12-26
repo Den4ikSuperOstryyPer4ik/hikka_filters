0470bb5c21e4458b15b0cbecd2861332981ca8f3
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2022-12-26 07:48:26 +0300

    Update __init__.py

--- hikka_filters/__init__.py
+++ hikka_filters/__init__.py
@@ -2,7 +2,7 @@ from .filters import *
 from .filters import __all__ as _all_
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __github__ = "http://github.com/Den4ikSuperOstryyPer4ik/hikka_filters"
 
 
